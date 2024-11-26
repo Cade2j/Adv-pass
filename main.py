7ba5bed0cd5e9320c2d44d0ba501f961cd618007
Cade J <171381047+Cade2j@users.noreply.github.com>
2024-11-26 12:07:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import hashlib
 #pnf = password not found
 pnf = True
-def hashtxt(text):
-    text_bytes = text.encode('utf-8')
-    hash_object = hashlib.sha256(text_bytes)
-    return hash_object.hexdigest()
+def hashtxt(txt):
+    return hash.sha256(txt.encode('utf-8')).hexdigest()
 while pnf:
     inp = input('Type your password: ')
     if hashtxt(inp) == "e0ecc78e7c4286e1f4556bc648c6d7ce5a60c9196c65ff02ae2c97d3f74811a7":
